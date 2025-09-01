92c5cb83156e856a31891a7e203ebea7af0b8428
ThalesOliveira-99 <thales930@gmail.com>
2025-09-01 13:12:36 -0300

    3° Parte

--- aulajpamaven/src/main/java/aplicação/Programa.java
+++ aulajpamaven/src/main/java/aplicação/Programa.java
@@ -1,18 +1,48 @@
 package aplicação;
 
 import dominio.Pessoa;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.PersistenceUnit;
 
 public class Programa {
 
 	public static void main(String[] args) {
-	
-		Pessoa p1 = new Pessoa(1, "Thales Oliveira", "thales@email.com");
-		Pessoa p2 = new Pessoa(2, "Tatiane Oliveira", "tatiane@email.com");
-		Pessoa p3 = new Pessoa(3, "William Custodio", "william@email.com");
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
 
+		// Instancia a configuração do persistence.xml
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+
+		// Com essa instanciação terá uma conexão com o banco de dados e toda persistência implementada
+		EntityManager em = emf.createEntityManager();
+		/*
+		 * //Iniciar a transação com o banco de Dados 
+		 * em.getTransaction().begin();
+		 * //persist é o que pega o método e joga para o banco de dados 
+		 * em.persist(p1);
+		 * em.persist(p2); 
+		 * em.persist(p3); 
+		 * //Confirmar a transação com o banco de Dados
+		 * em.getTransaction().commit();
+		 */
+		
+		//JPA tem uma busca pelo ID
+		/*
+		 * Pessoa p = em.find(Pessoa.class, 2);
+		 * 
+		 * System.out.println(p);
+		 */
+		
+		//JPA quando deseja apagar alguma informação do BD
+		Pessoa p = em.find(Pessoa.class, 2);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		
+		System.out.println("Finalizado, pai!");
+		
+		em.close();
+		emf.close();
 	}
 
 }
